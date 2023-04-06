18022945887154e7d071ddec519519c989d521f1
Özgün Deniz Gezerli <oezguen-deniz.gezerli@flixbus.com>
2023-04-06 15:27:51 +0200

    add main

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import ipdb
+# Load the training dataset
+ipdb.set_trace()
+train_df = pd.read_csv('data/train.csv')
+
+# Print the first 5 rows of the dataset
+print(train_df.head())
