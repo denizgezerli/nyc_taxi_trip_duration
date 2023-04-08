1b62b94199d1e88a8cbaec94bf0112abd041ad50
Özgün Deniz Gezerli <oezguen-deniz.gezerli@flixbus.com>
2023-04-08 12:27:35 +0200

    modify analysis notebook

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import ipdb
 # Load the training dataset
-ipdb.set_trace()
-train_df = pd.read_csv('data/train.csv')
+train_df = pd.read_csv('data/train.csv', on_bad_lines='skip')
 
 # Print the first 5 rows of the dataset
 print(train_df.head())
